fba018a86768ce24c8bbe5e24533199dc5e1d126
aalexia02 <aalexia02@gmail.com>
2025-09-26 17:05:30 +0200

    first commit

---
+++ Es01/configurazione2.py
@@ -0,0 +1,30 @@
+import tdwf # importiamo il modulo...
+import matplotlib.pyplot as plt
+import numpy as np
+import time 
+
+ad2 = tdwf.AD2() # connessione all’hardware
+ 
+ 
+scope = tdwf.Scope(ad2.hdwf) # inizializzazione oscilloscopio
+scope.fs = 1e6 # => frequenza di sampling impostata a 1MSa/s
+scope.npt = 1000 # => acquisizione impostata a 1000 punti
+ 
+scope.ch1.rng = 5 # range Ch1 su [-2.5,+2.5]
+scope.ch2.rng = 50 # range Ch2 su [-25,+25]
+ 
+ 
+scope.ch1.avg = True # attiva media su Ch2
+scope.sample() # Avvio acquisizione
+time.sleep(0.5)
+ 
+print(scope.ch1.vals) # stampa su shell 
+
+plt.plot(scope.time.vals, scope.ch1.vals, '-')
+plt.xlabel("Tempo [s]")
+plt.ylabel("Ch1 [V]")
+
+data = np.column_stack((scope.time.vals,scope.ch1.vals,scope.ch2.vals))
+np.savetxt("nomefile1.txt", data, delimiter="\t")
+
+plt.show()
